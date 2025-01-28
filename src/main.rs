9d0c15346ca21d59a8cf9488591decf1f47ee231
massivebird <33991373+massivebird@users.noreply.github.com>
2025-01-28 18:07:30 -0500

    added: print statement

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,12 @@ async fn main() {
 
     let app = app.fallback(|| async { (StatusCode::NOT_FOUND, "404") });
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    println!("listening on {}", listener.local_addr().unwrap());
+
     axum::serve(listener, app).await.unwrap();
 }
 
